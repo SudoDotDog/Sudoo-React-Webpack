795698ec5be773649de7b420310bbe848f0e4b1e
WMXPY <wm@mengw.io>
2020-08-07 13:03:17 -0500

    seprate svg generation

--- webpack/common/file.ts
+++ webpack/common/file.ts
@@ -4,11 +4,12 @@
  * @description File Loader
  */
 
+import * as SvgToMiniDataURI from "mini-svg-data-uri";
 import * as Webpack from "webpack";
 
 export const createUrlLoaders = (): Webpack.RuleSetRule[] => [
     {
-        test: /\.(webp|svg|png|jpe?g|gif)$/i,
+        test: /\.(webp|png|jpe?g|gif)$/i,
         use: [
             {
                 loader: 'url-loader',
@@ -16,6 +17,22 @@ export const createUrlLoaders = (): Webpack.RuleSetRule[] => [
                     fallback: 'file-loader',
                     limit: 8192,
                     name: '[contenthash].[ext]',
+                },
+            },
+        ],
+    },
+    {
+        test: /\.svg$/i,
+        use: [
+            {
+                loader: 'url-loader',
+                options: {
+                    fallback: 'file-loader',
+                    limit: 8192,
+                    name: '[contenthash].[ext]',
+                    generator: (content: any) => {
+                        return SvgToMiniDataURI(content.toString());
+                    },
                 },
             },
         ],
